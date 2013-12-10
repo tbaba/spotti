6f0e30625966f7bbc7a6fb3a18c91b6dce2f920a
Tatsuro Baba <harakirisoul@gmail.com>
2013-12-10 21:34:05 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131210123234) do
+
+  create_table "users", force: true do |t|
+    t.string   "user_name",    null: false
+    t.string   "handle_name"
+    t.string   "email",        null: false
+    t.string   "uid",          null: false
+    t.string   "provider"
+    t.string   "access_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
