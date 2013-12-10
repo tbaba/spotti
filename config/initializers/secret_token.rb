2d9c1c4cc21a0484286fe3d03749c8b74f9f5ed7
Tatsuro Baba <harakirisoul@gmail.com>
2013-12-10 13:48:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotti::Application.config.secret_key_base = '71a22ca38772e2a93c290353df7089422738c4729af535c219924956003a9a8b99126eb2ca60f681b7d25f2f42a491baa6de321cf367481356c8b4fa5c367046'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spotti::Application.config.session_store :cookie_store, key: '_spotti_session'
