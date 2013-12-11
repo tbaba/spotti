4325c7a1df90c0418288f855410b760753431ab2
Tatsuro Baba <harakirisoul@gmail.com>
2013-12-11 14:06:41 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210123234) do
+ActiveRecord::Schema.define(version: 20131211050600) do
 
   create_table "users", force: true do |t|
     t.string   "user_name",           null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20131210123234) do
     t.string   "access_token_secret"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
 end
