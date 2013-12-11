d0a7c4b44cfbafecf2e7b2dd8e27f563f17acf2f
Tatsuro Baba <harakirisoul@gmail.com>
2013-12-11 22:39:19 +0900

    User has many spots

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates_presence_of :user_name, :email, :uid, :access_token, :access_token_secret
 
   mount_uploader :avatar, AvatarUploader
+
+  has_many :spots
 end
